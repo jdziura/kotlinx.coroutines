dfbaf1a29d74f23c4f7cc409db10ac8341457680
Jakub Dziura <jdziura10@gmail.com>
2023-09-11 15:57:14 +0200

    Add HillClimbing Benchmark

---
+++ benchmarks/src/jmh/kotlin/benchmarks/scheduler/HillClimbingBenchmark.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package benchmarks.scheduler
+
+import benchmarks.*
+import benchmarks.common.*
+import kotlinx.coroutines.*
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.*
+
+
+@Warmup(iterations = 100, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 20, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+open class HillClimbingBenchmark : ParametrizedDispatcherBase() {
+
+    @Param("scheduler", "fjp", "go_scheduler", "dotnet_scheduler")
+    override var dispatcher: String = "fjp"
+
+    private val tasksWidth = 100
+    private val tasksDepth = 10
+    private val taskLenMinMs = 2L
+    private val taskLenMaxMs = 5L
+
+    @Benchmark
+    fun hillClimbingBenchmark() {
+        val latch = CountDownLatch(tasksWidth)
+        val jobs = ArrayList<Job>()
+
+        for (i in 1..tasksWidth) {
+            jobs += GlobalScope.launch(coroutineContext) {
+                repeat(tasksDepth) {
+                    launch(coroutineContext) {
+                        val time = ThreadLocalRandom.current().nextLong(taskLenMinMs, taskLenMaxMs + 1)
+                        doGeomDistrWork(50)
+                        Thread.sleep(time)
+                        doGeomDistrWork(50)
+                    }.join()
+                }
+
+                latch.countDown()
+            }
+        }
+
+        latch.await()
+    }
+}
